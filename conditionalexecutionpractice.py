04ae70006984622b76fbf2551a51f7e2aa34c8af
cmis <cmis@cmis-hs21.(none)>
2016-03-01 10:56:56 +0700

    added the practice

---
+++ conditionalexecutionpractice.py
@@ -0,0 +1,19 @@
+def beingLazy():
+    print "snore snore"
+def gettingToWork():
+    print "get up get up"
+def breakfastTime(): 
+    print "om nom nom" 
+
+stopSleeping = True
+beAwake = True
+goEat = True 
+
+if stopSleeping and beAwake and goEat:
+    beingLazy()
+    gettingToWork()
+    breakfastTime()
+
+print "Hello"
+
+
