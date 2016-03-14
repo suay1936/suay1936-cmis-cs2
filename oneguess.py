eb5e79495a664cb51f09f9522cb3f93926c20fab
cmis <cmis@cmis-hs00.(none)>
2016-03-14 10:55:37 +0700

    added oneguess

---
+++ oneguess.py
@@ -0,0 +1,28 @@
+# What is the minimum number? 5
+# What is the maximum number? 10
+# I'm thinking of a number from 5 to 10.
+# What do you think it is?: 7
+
+# The target was 9.
+# Your guess was 7.
+# That's under by 2.
+
+
+
+#need to use the if an comparison
+
+
+import math
+
+def minimumNumber
+
+
+
+def main():
+    minimumNumber = raw_input("What is the minimum number?")
+    maximumNumber = raw_input("What is the maximum number?")
+    rangeOfNumber = raw_input("I'm thinking of a number from")
+    myNumberGuess = raw_input("What do you think it is?: ")
+
+   
+    
