cf857d327e6abab72839904eacf7e62378ffa28b
cmis <cmis@cmis-hs14.(none)>
2016-03-17 08:41:11 +0700

    edited oneguess

--- oneguess.py
+++ oneguess.py
@@ -15,20 +15,31 @@
 import random 
 import math 
 
-def numberRange(minNumber, maxNumber): 
-	return 
-	
 
-	
-	if guess > target
-    print 
+def sub(guess, target):
+    return guess - target
+def sub(guess, target):
+    return target - guess
 
 
 def main():
     minNumber = int(raw_input("What is the minimum number? "))
     maxNumber = int(raw_input("What is the maximum number? "))
-    print "I'm thinking of a number from
+
+    print "I'm thinking of a number from" 
     myNumberGuess = int(raw_input("What do you think it is?: "))
+ 
+    def condition(guessTarget):
+	
+        if int(guess) > int(target) #needs work
+   	        print "That's over by" 
+            return sub(int(guess), int(target))
+        elif int(guess) < int(target)
+	        print "That's under by" sub(int(target), int(guess)) 
+        else int(guess) == int(target):
+             print "That's amazing! Someone's a lucky soul today!" 
+
+
 
 main()
    
