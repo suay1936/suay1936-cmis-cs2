504f77b0ca07aefa69583ec121471838bcd8f39a
cmis <cmis@cmis-hs00.(none)>
2016-03-24 08:40:27 +0700

    edited conditionals

--- conditionals.py
+++ conditionals.py
@@ -2,17 +2,49 @@
 # Write a description of what your script will do first. This description can be written in gedit and used as the basis of your code later. See this example.
 # name the file conditionals.py
 # 2 conditional execution 
-2 alternative execution 
-2 chained conditionals 
-Use raw_input.
-Use good variable names.
-Use type conversion when necessary.
-Define at least 4 functions that do something substantial (not just adding 2 numbers or multiplying). At least 1 must return a boolean value and be used as a part of the flow control. At least one must return an integer or float. At least 1 must return a string. The 4th can do what ever you want.
-Define a main() function to organize the flow of your program. Be sure to think about the three phases (input, processing, output)
-Use at least 3 different relational operators.
-Use each of the logical operators at least once. 
-Use random.random() and random.randint() at least once each in your script.
-Use str.format() at least once in your script.
-Use """ or ''' strings at least once in your script.
-Write an interesting and/or entertaining story/game.
-Use the same structure we learned about in the simple program assignment.
+#2 alternative execution 
+#2 chained conditionals 
+#Use raw_input.
+#Use good variable names.
+#Use type conversion when necessary.
+#Define at least 4 functions that do something substantial (not just adding 2 numbers or multiplying). At least 1 must return a boolean value and be used as a part of the flow control. At least one must return an integer or float. At least 1 must return a string. The 4th can do what ever you want.
+#Define a main() function to organize the flow of your program. Be sure to think about the three phases (input, processing, output)
+#Use at least 3 different relational operators.
+#Use each of the logical operators at least once. 
+#Use random.random() and random.randint() at least once each in your script.
+#Use str.format() at least once in your script.
+#Use """ or ''' strings at least once in your script.
+#Write an interesting and/or entertaining story/game.
+
+#My Game
+# My game that will tell you at what age you will die and how you will die
+# It will ask for your current age
+# It will ask you if you have ever been in any accidents that require multiple days in the hospital and what age were you when this happened
+# It will ask you if you have participated in any extreme activities or would like to
+# find a list of extreme activities: bungee jumping, creeking, 
+# It will ask you questions about how you spend your daily life
+# It will ask you how you plan to spend your future; where you want to go; what you want to do
+
+
+
+
+# It 
+#Use the same structure we learned about in the simple program assignment.
+
+
+
+
+import random
+import math
+
+def thePast(accidents):
+	return """ 
+accident """.format("Were you in any accidents that required multiple days of stay in the hospital" (y) == (n) 
+
+	raw_input
+
+
+
+
+
+
