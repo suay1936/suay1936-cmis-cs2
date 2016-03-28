741518159796a7da3f1a162740a6a3be9a4a5f76
cmis <cmis@cmis-hs16.(none)>
2016-03-28 10:50:49 +0700

    edited simple program

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,27 +1,29 @@
-#g = GBP, d = USD, b = THB
+
 #mul conversion
-#d1= amount of US dollars
-#p1= 0.7*d1
-#b1= 35.63*d1
-#p2= amount of British pounds
-#d2= 1.43*p2
-#b2= 50.94*p2
-#b3= amount of Thai baht
-#d3= 0.028*b3
-#p3= 0.020*b3
+#dollar1= amount of US dollars
+#pound1= 0.7*d1
+#baht1= 35.63*d1
+#pound2= amount of British pounds
+#dollar2= 1.43*p2
+#baht2= 50.94*p2
+#baht3= amount of Thai baht
+#dollar3= 0.028*b3
+#pound3= 0.020*b3
 #add total amount of thai baht 
-#b4= b1 + b2 + b3
-#d4 = d1 + d2 +d3
-#p4 = p1 + p2 +p3 
-
-#find the wavelength with only a few information given; use the physics formula that has the squareroot thing 
-
-#print the 4 sentences below using message box for each of the sentences 
-
-#Well, you had (amount of usd ) USB but now you have (amount of thb) THB and (amount of gbp) GBP.
-#Well, you had (amount of gbp) GBP but now you have (amount of thb) THB and (amount of usd) USD.
-#Well, you had (amount of thb) THB but now you have (amount of usd) USD and (amount of gbp) GBP. 
-#From what you’ve put in you have a total of (total amount of THB) and (total amount of USD) and (total amount of GBP)! Don’t shop too much! 
+#baht4= b1 + b2 + b3
+#dollar4 = d1 + d2 +d3
+#pound4 = p1 + p2 +p3 
+# str = i have .. b .. p.. d
 
 
-def mul ( 
+import math
+
+def 
+
+def total(baht4, dollar4, pound4):
+    return
+
+def main():
+    totalBaht = raw_input("How much money do you have in baht?: ")
+    
+
