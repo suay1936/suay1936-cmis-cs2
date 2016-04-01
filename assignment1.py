285c87831586828644b8c040c872542979f2e3a9
cmis <cmis@cmis-hs16.(none)>
2016-04-01 10:51:31 +0700

    edited assignment1

---
+++ assignment1.py
@@ -0,0 +1,32 @@
+myName= "KS"  # myName is the first variable used
+print myName
+myAge= 18 # this is an integer
+print myAge
+myAgeInDecimal= float(5 / 12) + 18  # needs fixing 
+print myAgeInDecimal
+myHeightInMeters = 1.56 # this is an 
+print myHeightInMeters
+squaresidelength = 4 # this is an integer
+print squaresidelength
+rectanglelength = 28 # this is another integer
+print rectanglelength
+rectangleheight = rectanglelength - 8 # this is an operation done in interactive mode 
+print rectangleheight
+myAgeInMonths = myAgeInDecimal * 12 # an operand was used to calculate the product
+print myAgeInMonths
+myYearsToLive = 80 - myAge # this is an expression
+print myYearsToLive
+myHeightInFeet = myHeightInMeters * 3.3 # this is an expression
+print myHeightInFeet
+myHeightDifferenceInMeters = 1.59 - myHeightInMeters # this is an expression with float numbers
+print myHeightDifferenceInMeters
+areaofsquare = squaresidelength ** 2 # two operands were used to calculate the area of the square
+print areaofsquare
+halfvolumeofcube = squaresidelength ** 3 / 2
+print halfvolumeofcube
+oneninthrectanglearea = float(1 / 9) * (rectanglelength * rectangleheight)
+print oneninthrectanglearea #needs fixing 
+#Print out a message using 5 of the variables you created. Use the string concatenation operator (+) to do this.
+#Print out a second message using 5 more of the variables you created. Use the comma this time to print multiple values.
+
+
