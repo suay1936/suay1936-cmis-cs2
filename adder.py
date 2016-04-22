352c4cb481a157e7920c4b717ccb918c84f72ab6
cmis <cmis@cmis-hs13.(none)>
2016-04-22 13:46:53 +0700

    added adder

---
+++ adder.py
@@ -0,0 +1,10 @@
+def adder(
+
+
+
+#adder stop case = print nothing then press enters gives your the sum
+#code must ask the user to put in some numbers if the user doesn't put in a number and presses enter >> adder stop case
+#
+
+#pass makes it do nothing 
+
