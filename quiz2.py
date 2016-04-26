66f1892b01681f254c446dcd9c01aeacccf88abc
cmis <cmis@cmis-hs07.(none)>
2016-04-26 08:42:41 +0700

    marked quiz 2

--- quiz2.py
+++ quiz2.py
@@ -1,37 +1,37 @@
 #PART 1: Terminology
 #1) Give 3 examples of boolean expressions.
-#a) 22 == 22
-#b) 22 == 34
-#c) 8 > 5 and 5 > 3
-#
+#a) 22 == 22 #point
+#b) 22 == 34 #point
+#c) 8 > 5 and 5 > 3 #point
+# 
 #2) What does 'return' do?
-# Return executes the function that has been defined in the function definition with the def 
+# Return executes the function that has been defined in the function definition with the def #point
 #
 #
 #
 #3) What are 2 ways indentation is important in python code?
-#a) It tells that the indented portion is part of the defined function
-#b) It tells the program when to run the code eg. you define a function and the indented in return will produce the output of the defined function
+#a) It tells that the indented portion is part of the defined function #point
+#b) It tells the program when to run the code eg. you define a function and the indented in return will produce the output of the defined function #point
 #
 #
 
 #PART 2: Reading
 #Type the values for 12 of the 16 of the variables below.
 #
-#problem1_a) 36
-#problem1_b) root 3
-#problem1_c) 0
-#problem1_d) -5
+#problem1_a) 36 #no point
+#problem1_b) root 3 #no point
+#problem1_c) 0 #point
+#problem1_d) -5 #point
 #
-#problem2_a) True
-#problem2_b) False
-#problem2_c) True
-#problem2_d) False
+#problem2_a) True #point
+#problem2_b) False #point
+#problem2_c) True #no point
+#problem2_d) False #no point
 #
-#problem3_a) 0.3
-#problem3_b) 0.5
-#problem3_c) 1
-#problem3_d) 0.5
+#problem3_a) 0.3 #point
+#problem3_b) 0.5 #point
+#problem3_c) 1 #no point
+#problem3_d) 0.5 #point
 #
 #problem4_a)
 #problem4_b)
@@ -71,4 +71,4 @@ def main():
 
 
 main()
-
+# 23 25 26 
