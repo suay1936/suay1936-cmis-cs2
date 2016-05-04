9037ae3675fffa4e290b3a277e6cd4b602661ca4
cmis <cmis@cmis-hs10.(none)>
2016-05-04 13:35:49 +0700

    added new recursion

---
+++ newrecursion.py
@@ -0,0 +1,6 @@
+
+#asks you to guess the number only up t5 numbers if not correct moves on to the next round
+#must tell you if the guessed number is too low or too high
+#takes up 5 rounds
+# recursion1: asks you for 5 numbers recursion 2: takes only 5 rounds
+
