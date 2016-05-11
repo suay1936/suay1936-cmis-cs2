0f3734237e425b4f4b674bd89762fc9c3fcdf9fc
cmis <cmis@cmis-hs08.(none)>
2016-05-11 13:34:47 +0700

    added new conditional

---
+++ conditionals1.py
@@ -0,0 +1,15 @@
+#random.random = 0 to 1
+
+def (current_age, accidents, daily_life, future_plans):
+    
+
+
+
+def main():
+
+    current_age = raw_input("How old are you?: ")
+    accidents = raw_input("Were you in any accidents that required multiple days of stay in the hospital?: ")
+    daily_life = raw_input("On avergae how many times a month do you get into accidents: ") 
+    future_plans = raw_input("Do you plan to participate in extreme activities in the future?: ")
+
+main()
