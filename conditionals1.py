351dc3890e863ebeaf92204854309ad4ebe08269
cmis <cmis@cmis-hs08.(none)>
2016-05-13 08:40:29 +0700

    edited conditionals

--- conditionals1.py
+++ conditionals1.py
@@ -1,15 +1,43 @@
-#random.random = 0 to 1
+# 2 conditional execution
+# 2 alternative execution
+# 2 chained conditonals
+# raw input
+# 4 def 1 bool 1 int 1 str 1 whatever you want 
+# main
+# comparison conditionals
+# or / not conditionals
+# random.random = 0 to 1
+# random.randint()
+# str.format()
+# """ 
 
-def (current_age, accidents, daily_life, future_plans):
-    
 
 
-
-def main():
+def asking_info(): #raw input
 
     current_age = raw_input("How old are you?: ")
     accidents = raw_input("Were you in any accidents that required multiple days of stay in the hospital?: ")
+    days_inHos = raw_input("How many days were you in the hospital?: ")
     daily_life = raw_input("On avergae how many times a month do you get into accidents: ") 
-    future_plans = raw_input("Do you plan to participate in extreme activities in the future?: ")
+    future_plans = raw_input("Do you plan to participate in extreme activities in the future? What are they: ")
 
+def dying_age(age): # int
+    age = current_age 
+    new_age = int(current_age) + 15 
+    return new_age
+    
+def accidents_proba(): # bool 
+    if int(days_inHos) >= 5
+        return "Your life will be shortened by 3 years, just kidding! :) "
+
+def future_thoughts():
+    
+    
+    
+    
+
+def main(): # main 
+
+    
+     
 main()
