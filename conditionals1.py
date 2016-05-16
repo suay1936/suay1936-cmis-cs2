8d217bfd3ae1e09d727043064aefd4946766cba3
cmis <cmis@cmis-hs08.(none)>
2016-05-16 10:55:41 +0700

    edited conditionals

--- conditionals1.py
+++ conditionals1.py
@@ -1,6 +1,6 @@
-# 2 conditional execution
-# 2 alternative execution
-# 2 chained conditonals
+# 2 conditional execution: if 
+# 2 alternative execution: if else 
+# 2 chained conditonals: if else elif 
 # raw input
 # 4 def 1 bool 1 int 1 str 1 whatever you want 
 # main
@@ -11,33 +11,42 @@
 # str.format()
 # """ 
 
-
-
-def asking_info(): #raw input
-
-    current_age = raw_input("How old are you?: ")
-    accidents = raw_input("Were you in any accidents that required multiple days of stay in the hospital?: ")
-    days_inHos = raw_input("How many days were you in the hospital?: ")
-    daily_life = raw_input("On avergae how many times a month do you get into accidents: ") 
-    future_plans = raw_input("Do you plan to participate in extreme activities in the future? What are they: ")
-
-def dying_age(age): # int
+   
+def dying_age(age): # int # bool
     age = current_age 
-    new_age = int(current_age) + 15 
+    new_age = int(current_age) + random.randint(5, 65)
     return new_age
     
-def accidents_proba(): # bool 
-    if int(days_inHos) >= 5
-        return "Your life will be shortened by 3 years, just kidding! :) "
+def accidents_proba(): 
+    days_inHos = raw_input("Were you in any accidents that required multiple days of stay in the hospital? How many days were you in the hospital?: ")
+    if int(days_inHos) >= 5: 
+        return "Your life will be shortened by", random.random() "years, just kidding! :) "
+    newer_age = int(new_age) - random.random()
+    else:
+        return random.randint(0, 100) 
+                newer_age = int(new_age) - random.random()
+def normal_life():
+    daily_life = raw_input("On avergae how many times a month do you get into accidents: ") 
+    if daily_life = 1:
+        return "You're quite a normal person"
+    elif daily_life >= 1: 
+        return "You should stay away from certain things"
+    elif daily_life >= 2:
+        return "You should be home for a few days"
+    elif daily_life >= 3:
+        return "You should be home for a few weeks" 
 
 def future_thoughts():
+     future_plans = raw_input("Do you plan to participate in extreme activities in the future? What are they: ")
     
-    
-    
-    
+def 
 
 def main(): # main 
 
+    current_age = raw_input("How old are you?: ")
+    dying_age(age)
+
     
+        
      
 main()
