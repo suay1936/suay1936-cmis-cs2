a541077956e31837514045e9fa8af4053cf047f8
cmis <cmis@cmis-hs08.(none)>
2016-05-17 13:10:24 +0700

    added quiz 3

---
+++ cs2quiz3.py
@@ -0,0 +1,73 @@
+#Section 1: Terminology
+# 1) What is a recursive function?
+#A recursive function will continue on producing an output unless there is a condition to make it stop.
+#
+#
+# 2) What happens if there is no base case defined in a recursive function?
+#The recursive will continue on recursing that function.
+#
+#
+# 3) What is the first thing to consider when designing a recursive function?
+#You consider the base case function.
+#
+#
+# 4) How do we put data into a function call?
+#You type it in the parentheses of function. 
+#
+# 
+# 5) How do we get data out of a function call?
+#You get data out of a function call by giving a variable to the data from the function call that is to be called. 
+#
+#
+
+#Section 2: Reading
+# Read the following function definitions and function calls.
+# Then determine the values of the variables q1-q20.
+
+#a1 = 1 + (2, 5)
+#a2 = 1 + (6, 1)
+#a3 = 1 + (-1, -2)
+
+#b1 = 2
+#b2 = 1 + (1, 0)
+#b3 = 1 + (-1, 0)
+
+#c1 = 1 + (-2, 2)
+#c2 = 4
+#c3 = 10 + (5, 4)
+
+#d1 = 6
+#d2 = 1 + (3, 1, 3)
+#d3 = 1 - (2, 2, 1)
+
+#Section 3: Programming
+#Write a script that asks the user to enter a series of numbers.
+#When the user types in nothing, it should return the average of all the odd numbers
+#that were typed in. 
+#In your code for the script, add a comment labeling the base case  on the line BEFORE the base case.
+#Also add a comment label BEFORE the recursive case.
+#It is NOT NECESSARY to print out a running total with each user input.
+
+
+# base case
+def average():
+    if num == "":
+        print "lol"
+
+# recursive case 
+def summation(num):
+    num = raw_input("Next number: ")
+    if num == "":
+        return avg 
+    else:
+        return num 
+
+
+
+def odd(num):
+    if num / 2 == float(num)
+        
+   
+        
+
+
