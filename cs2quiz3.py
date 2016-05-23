f0f0a6333e71471cdfd226f3805447a6a450b7f0
cmis <cmis@cmis-hs08.(none)>
2016-05-23 10:46:52 +0700

    marked quiz 3

--- cs2quiz3.py
+++ cs2quiz3.py
@@ -1,22 +1,22 @@
 #Section 1: Terminology
 # 1) What is a recursive function?
-#A recursive function will continue on producing an output unless there is a condition to make it stop.
+#A recursive function will continue on producing an output unless there is a condition to make it stop. #point
 #
 #
 # 2) What happens if there is no base case defined in a recursive function?
-#The recursive will continue on recursing that function.
+#The recursive will continue on recursing that function. #point
 #
 #
 # 3) What is the first thing to consider when designing a recursive function?
-#You consider the base case function.
+#You consider the base case function.#point
 #
 #
 # 4) How do we put data into a function call?
-#You type it in the parentheses of function. 
+#You type it in the parentheses of function. #point
 #
 # 
 # 5) How do we get data out of a function call?
-#You get data out of a function call by giving a variable to the data from the function call that is to be called. 
+#You get data out of a function call by giving a variable to the data from the function call that is to be called. #no point
 #
 #
 
@@ -24,21 +24,21 @@
 # Read the following function definitions and function calls.
 # Then determine the values of the variables q1-q20.
 
-#a1 = 1 + (2, 5)
-#a2 = 1 + (6, 1)
+#a1 = 1 + (2, 5) no point
+#a2 = 1 + (6, 1) 
 #a3 = 1 + (-1, -2)
 
 #b1 = 2
-#b2 = 1 + (1, 0)
-#b3 = 1 + (-1, 0)
+#b2 = 1 + (1, 0) no
+#b3 = 1 + (-1, 0) no
 
-#c1 = 1 + (-2, 2)
-#c2 = 4
-#c3 = 10 + (5, 4)
+#c1 = 1 + (-2, 2) no
+#c2 = 4 
+#c3 = 10 + (5, 4) no 
 
 #d1 = 6
-#d2 = 1 + (3, 1, 3)
-#d3 = 1 - (2, 2, 1)
+#d2 = 1 + (3, 1, 3) no
+#d3 = 1 - (2, 2, 1) no
 
 #Section 3: Programming
 #Write a script that asks the user to enter a series of numbers.
@@ -48,7 +48,7 @@
 #Also add a comment label BEFORE the recursive case.
 #It is NOT NECESSARY to print out a running total with each user input.
 
-
+#point = 4
 # base case
 def average():
     if num == "":
