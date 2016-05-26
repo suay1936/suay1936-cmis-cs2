4c80c3d5ef672985a7ab01c1b11644ae45b4639c
cmis <cmis@cmis-hs17.(none)>
2016-05-26 08:44:03 +0700

    edited

--- whilelooppractice.py
+++ whilelooppractice.py
@@ -1,3 +1,5 @@
+
+
 #while  True:  #infinite smiley faces
     #print ";)",
 
@@ -20,21 +22,37 @@ def countup(c):
         c += 2 
 
 
-
 def count(d):
     while d > 0:
         print d 
         d -= 1
-    while d < 0:
-        print d
+        while d < 0:
+            print d
+
         d += 1
-    while d == 0:
-        print " " 
+        while d == 0:
+                print "Nahhh" 
+
+
+#sum of all odds from n to 0
+
+def addOdds(n):
+    if n > 0:     
+        while n > 0:
+            print n % 2 == 0 
+                n -= 1
+    elif n < 0:
+        while n < 0:
+            print n % 2 == 0
+                n += 1
+    
+         
 
 
 def main():
     countdown(12)
     countup(12)
     count(10)
+    
 main()
 
