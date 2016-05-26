bdf28239a7bcc62ce7e766641f4c966d3044fddd
cmis <cmis@cmis-hs17.(none)>
2016-05-26 08:43:18 +0700

    added another while loop prac.

---
+++ otherwhileloopprac.py
@@ -0,0 +1,16 @@
+def addOdds(n):
+    if n > 0:     
+        while n >= 0:
+            print n % 2 == 0
+            n += 1
+    elif n < 0:
+        while n <= 0:
+            print n % 2 == 0
+            n -= 1
+
+
+
+
+
+
+
