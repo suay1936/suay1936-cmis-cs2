2edd92f456e44f5e015c87256c356ed407b1053a
cmis <cmis@cmis-hs07.(none)>
2016-06-02 14:27:54 +0700

    edited

--- conditionals1.py
+++ conditionals1.py
@@ -12,7 +12,7 @@
 # """ 
 
    
-def dying_age(age): # int # bool
+def dying_age(age): 
     age = current_age 
     new_age = int(current_age) + random.randint(5, 65)
     return new_age
@@ -20,33 +20,52 @@ def dying_age(age): # int # bool
 def accidents_proba(): 
     days_inHos = raw_input("Were you in any accidents that required multiple days of stay in the hospital? How many days were you in the hospital?: ")
     if int(days_inHos) >= 5: 
-        return "Your life will be shortened by", random.random() "years, just kidding! :) "
+        return ("Your life will be shortened by: ", random.randint, "JK!: ")
     newer_age = int(new_age) - random.random()
-    else:
-        return random.randint(0, 100) 
-                newer_age = int(new_age) - random.random()
+    else :
+        random.randint(0, 100)
+        newer_age = int(new_age) - random.random()
+            return newer_age
 def normal_life():
     daily_life = raw_input("On avergae how many times a month do you get into accidents: ") 
-    if daily_life = 1:
+    if daily_life = 0:
         return "You're quite a normal person"
-    elif daily_life >= 1: 
+    elif daily_life = 1: 
         return "You should stay away from certain things"
-    elif daily_life >= 2:
+    elif daily_life = 2:
         return "You should be home for a few days"
     elif daily_life >= 3:
-        return "You should be home for a few weeks" 
+        return "You should be home for a long long time" 
 
 def future_thoughts():
      future_plans = raw_input("Do you plan to participate in extreme activities in the future? What are they: ")
-    
-def 
+        print future_plans
+
+def other_things():
+        stuffs = ("I like to swim ", random.randint(32, 450), "a day.: ")
+
+def velocity():
+        distace = random.random(0, 1000)
+        time = random.randint
+        actual_velocity = float(random.random) / float(random.randint
+            return actual_velocity 
+            if velocity = 1000/0.1
+                return "Winning on the way"
+                else:
+                    return " "
+def acceleration():
+        
+        
+
+
 
 def main(): # main 
 
     current_age = raw_input("How old are you?: ")
     dying_age(age)
-
-    
-        
+    normal_life(daily_life)
+    future_thoughts(future_plans)
+    other_things(stuffs)
+    velocity(actual_velocity)    
      
 main()
