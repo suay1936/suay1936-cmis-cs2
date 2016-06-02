02d9f78295e2e13774684808bd1f67ba63fe1cd8
cmis <cmis@cmis-hs07.(none)>
2016-06-02 13:45:52 +0700

    edited

--- conditionals.py
+++ conditionals.py
@@ -1,36 +1,29 @@
-# Make a game that 
-# Write a description of what your script will do first. This description can be written in gedit and used as the basis of your code later. See this example.
-# name the file conditionals.py
-# 2 conditional execution 
-#2 alternative execution 
-#2 chained conditionals 
-#Use raw_input.
-#Use good variable names.
-#Use type conversion when necessary.
-#Define at least 4 functions that do something substantial (not just adding 2 numbers or multiplying). At least 1 must return a boolean value and be used as a part of the flow control. At least one must return an integer or float. At least 1 must return a string. The 4th can do what ever you want.
-#Define a main() function to organize the flow of your program. Be sure to think about the three phases (input, processing, output)
-#Use at least 3 different relational operators.
-#Use each of the logical operators at least once. 
-#Use random.random() and random.randint() at least once each in your script.
-#Use str.format() at least once in your script.
-#Use """ or ''' strings at least once in your script.
-#Write an interesting and/or entertaining story/game.
-#My Game
-# My game that will tell you at what age you will die and how you will die
-# It will ask for your current age
-# It will ask you if you have ever been in any accidents that require multiple days in the hospital and what age were you when this happened
-# It will ask you if you have participated in any extreme activities or would like to
-# find a list of extreme activities: bungee jumping, creeking, 
-# It will ask you questions about how you spend your daily life
-# It will ask you how you plan to spend your future; where you want to go; what you want to do
-#Use the same structure we learned about in the simple program assignment.
 
+
+# 2 conditional execution: if 
+# 2 alternative execution: if else 
+# 2 chained conditonals: if else elif 
+# raw input
+# 4 def 1 bool 1 int 1 str 1 whatever you want 
+# main
+# comparison conditionals
+# or / not conditionals
+# random.random = 0 to 1
+# random.randint()
+# str.format()
+# """ 
+# game that calculates the velocity dxt
+# raw input to ask for distance 
+# use random to generate random time 
+# compare to acceleration of something use random to generate the number for the calculation v(from above) - v(random) over t 
 
 
 
 import random
 import math
 
+def velocity():
+    distance = raw_input("How far has the car gone so far?: ")        
 
 
 
@@ -38,20 +31,27 @@ import math
 
 
 
-def accidents(yes, no):
-	if accidents == yes
-        print int(current_age) + 15 
-        
 
- 
+
+
+
+
+
+
+
+
+
 
 def main():
-    current_age = raw_input("How old are you?: ")
-    accidents = raw_input("Were you in any accidents that required multiple days of stay in the hospital?: ")
-    daily_life = raw_input("On avergae how many times a month do you get into accidents") 
-    future_plans = raw_input("Do you plan to participate in extreme activities in the future: ")
 
 
 
-main() 
+main()
+
+
+
+
+
+
+
 
